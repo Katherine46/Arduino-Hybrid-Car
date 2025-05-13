6df5e23dffaa7d521fcd8069435e92e1bb0388c3
Katherine <joycehutagaol232010@gmail.com>
2025-05-13 17:28:14 +0700

    Update drone_mode.h

--- include/drone_mode.h
+++ include/drone_mode.h
@@ -1,6 +1,18 @@
 #ifndef DRONE_MODE_H
 #define DRONE_MODE_H
 
-void DroneMode();
+#include <Arduino.h>
+
+// Fungsi inisialisasi drone mode
+void initDroneMode();
+
+// Fungsi loop utama untuk drone mode
+void handleDroneCommand(char command);
+
+// Fungsi-fungsi untuk kontrol dasar
+void takeOff();
+void land();
+void increaseAltitude();
+void decreaseAltitude();
 
 #endif
